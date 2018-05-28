08955782ab39106a277b7db0e70b3027a5e483ba
Andres Felipe Ocampo Eljaiesk <aocampoe@everis.com>
2018-05-28 18:03:55 +0200

    first commit

---
+++ AA_TextField/AATextFieldEffects.swift
@@ -0,0 +1,9 @@
+//
+//  AATextFieldEffects.swift
+//  AA_TextField
+//
+//  Created by Andres Felipe Ocampo Eljaiesk on 28/5/18.
+//  Copyright © 2018 icologic. All rights reserved.
+//
+
+import Foundation
